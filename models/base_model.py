f8fd85e5f17bb88db85a1c51bd4011607d25ffc3
Rachel Lewis <RLewis11769@gmail.com>
2021-05-18 01:37:14 +0000

    Add test info for FileStorage class

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,9 @@
 
 """ Module for BaseModel class """
 
-
 from uuid import uuid4
 from datetime import datetime
+from models.__init__ import storage
 
 
 class BaseModel:
@@ -16,13 +16,15 @@ class BaseModel:
             for key in kwargs.keys():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f"))
+                        setattr(self, key, datetime.
+                                strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f"))
                     else:
                         setattr(self, key, kwargs[key])
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ Returns string representation of instance """
@@ -32,6 +34,7 @@ class BaseModel:
     def save(self):
         """ Updates updated_at when saved """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ Creates dictionary in json format of attributes """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" Module for FileStorage class """
+
+
+import json
+
+
+class FileStorage():
+    """ Methods and attributes for FileStorage class """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Returns dictionary objects """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets obj with key <class_name>.id """
+        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+
+    def save(self):
+        """ Serializes __objects dictionary to json file """
+        with open(FileStorage.__file_path, 'w') as f:
+            tmp = {}
+            for key in FileStorage.__objects.keys():
+                tmp[key] = FileStorage.__objects[key].to_dict()
+            json.dump(tmp, f)
+
+    def reload(self):
+        """ Deserializes json file to __objects dictionary """
+        try:
+            with open(FileStorage.__file_path) as f:
+                from models.base_model import BaseModel
+                FileStorage.__objects = {}
+                tmp = json.load(f)
+                for key in tmp:
+                    if key[:9] == "BaseModel":
+                        FileStorage.__objects[key] = BaseModel(**tmp[key])
+        except:
+            pass
