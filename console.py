6d8c2dc9a79025646c828a8409d7e3d6017ed756
JakeFC <jacobchavera@yahoo.com>
2021-05-18 19:42:21 +0000

    basic console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for HBNBCommand class"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter subclass with class attribute: prompt
+    and public instance methods: EOF and quit"""
+    prompt = '(hbnb)'
+
+    def do_EOF(self, arg):
+        """exits the program"""
+        print()
+        return True
+
+    def do_quit(self, arg):
+        """exits the prgram"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
